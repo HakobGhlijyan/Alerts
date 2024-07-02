647cbc90e0eb42e086d0446e25ac2b4139aee58a
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-07-02 13:17:01 +0400

    Initial Commit

---
+++ Alerts/AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsApp.swift
+//  Alerts
+//
+//  Created by Hakob Ghlijyan on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
